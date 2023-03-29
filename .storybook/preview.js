e433e6729ece43764e2c971f94e756a9e073aafb
vikas-cldcvr <vikas@cldcvr.com>
2023-03-29 17:11:53 +0530

    FLOW-721 menulist story converted to vue

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,3 @@
-import { html } from "lit-html";
 import SystemIconPack from "@cldcvr/flow-system-icon/dist/types/icon-pack";
 import ProductIconPack from "@cldcvr/flow-product-icon/dist/types/icon-pack";
 import GcpIconPack from "@cldcvr/flow-gcp-icon/dist/types/icon-pack";
@@ -9,63 +8,48 @@ import "./storybook.css";
 import { ConfigUtil } from "@cldcvr/flow-core";
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  backgrounds: { disable: true },
-  //   themes: {
-  //     default: "f-light",
-  //     clearable: false,
-  //     list: [
-  //       { name: "f-dark", color: "#000" },
-  //       { name: "f-light", color: "#fff" },
-  //     ],
-  //   },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-  previewTabs: {
-    "storybook/docs/panel": { index: -1 },
-  },
+	actions: { argTypesRegex: "^on[A-Z].*" },
+	backgrounds: { disable: true },
+	//   themes: {
+	//     default: "f-light",
+	//     clearable: false,
+	//     list: [
+	//       { name: "f-dark", color: "#000" },
+	//       { name: "f-light", color: "#fff" },
+	//     ],
+	//   },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/
+		}
+	},
+	previewTabs: {
+		"storybook/docs/panel": { index: -1 }
+	}
 };
 
 export const decorators = [
-  (story) => {
-    window.onmessage = function (e) {
-      if (e.data && typeof e.data === "string") {
-        const message = JSON.parse(e.data);
+	() => {
+		window.onmessage = function (e) {
+			if (e.data && typeof e.data === "string") {
+				const message = JSON.parse(e.data);
 
-        if (message.event.type === "storybook-addon-themes/change") {
-          ConfigUtil.setConfig({ theme: message.event.args[0] });
-        }
-      }
-    };
-    ConfigUtil.setConfig({
-      iconPack: {
-        ...SystemIconPack,
-        ...ProductIconPack,
-        ...GcpIconPack,
-        ...AwsIconPack,
-      },
-    });
-    return html`
-      <style>
-        .sb-show-main.sb-main-padded {
-          background-color: var(--color-surface-default);
-          padding: 0;
-        }
-        #root,
-        #root-inner {
-          height: 100%;
-        }
-        body {
-          overflow: auto;
-        }
-      </style>
-      <div style="color:var(--color-text-default);font-family:var(--flow-font);height:inherit;">
-        ${story()}
-      </div>
-    `;
-  },
+				if (message.event.type === "storybook-addon-themes/change") {
+					ConfigUtil.setConfig({ theme: message.event.args[0] });
+				}
+			}
+		};
+		ConfigUtil.setConfig({
+			iconPack: {
+				...SystemIconPack,
+				...ProductIconPack,
+				...GcpIconPack,
+				...AwsIconPack
+			}
+		});
+		return {
+			template: `<div style="color:var(--color-text-default);font-family:var(--flow-font);height:inherit;"><story/></div>`
+		};
+	}
 ];
