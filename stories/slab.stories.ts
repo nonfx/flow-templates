36f9ec3464b7e261e1dd446203a8469cb8bff699
Mayank fulera <fuleramayank@gmail.com>
2023-04-04 08:40:50 +0530

    slab

--- stories/slab.stories.ts
+++ stories/slab.stories.ts
@@ -14,16 +14,17 @@ export default {
 export const SingleLineSlab01 = () =>
 	defineComponent({
 		template: /*html*/ `
-          <f-div width="382px" state="secondary" padding="medium" gap="medium" align="middle-left">
+    <f-div>
+          <f-div width="382px" state="secondary" padding="medium" :gap="'medium'" align="middle-left">
              <!--Start : slab-left -->
             <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
               <f-icon source="i-icon"></f-icon>
             </f-div>
              <!--SEnd : slab-left -->
              <!--Start : slab-middle  -->
-            <f-div data-f-id="slab-middle" width="fill-container" gap="medium" align="middle-left">
+            <f-div data-f-id="slab-middle" width="fill-container" :gap="'medium'" align="middle-left">
               <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
-              <f-div gap="medium" width="hug-content">
+              <f-div :gap="'medium'" width="hug-content">
                 <f-icon-button
                   icon="i-icon"
                   variant="block"
@@ -73,75 +74,306 @@ export const SingleLineSlab01 = () =>
               ></f-icon-button>
             </f-div>
             <!--End : slab-right -->
+          </f-div>
           </f-div>`
 	});
 
-export const text = () =>
+export const SingleLineSlab02 = () =>
 	defineComponent({
-		template: /*html*/ `<f-div padding="large" align="middle-center" direction="column">
-        <!--Start : menu-list -->
-          <f-div width="220px" state="secondary" padding="medium" gap="medium" border="small solid default bottom" clickable>
-            <!--Start : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-left" gap="medium" align="middle-left">
-              <f-text variant="para" size="medium" weight="regular">Text</f-text>
+		template: /*html*/ `
+            <!--Start : single-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium" align="middle-left">
+             <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon"></f-icon>
             </f-div>
-            <!--End : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
+             <!--SEnd : slab-left -->
+             <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" width="fill-container" gap="medium" align="middle-left">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
+              <f-div gap="medium" width="hug-content">
+                <f-counter label="88"></f-counter>
+              </f-div>
+            </f-div>
+            <!--End : slab-middle  -->
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+            </f-div>
+            <!--End : slab-right -->
           </f-div>
-        <!--End : menu-list -->
-        </f-div>`
+        <!--End : single-line slab -->
+    `
 	});
 
-export const icon_text = () =>
+export const SingleLineSlab03 = () =>
 	defineComponent({
-		template: /*html*/ `<f-div padding="large" align="middle-center">
-        <!--Start : menu-list -->
-          <f-div width="220px" state="secondary" padding="medium" gap="medium" border="small solid default bottom" clickable>
-            <!--Start : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-left" gap="medium" align="middle-left">
-                <f-icon size="small" source="i-icon"></f-icon>
-              <f-text variant="para" size="medium" weight="regular">Text</f-text>
+		template: /*html*/ `
+            <!--Start : single-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium" align="middle-left">
+             <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon"></f-icon>
             </f-div>
-            <!--End : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
+             <!--SEnd : slab-left -->
+             <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" width="fill-container" gap="medium" align="middle-left">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
+               <f-div gap="medium" width="hug-content">
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+              </f-div>
+            </f-div>
+            <!--End : slab-middle  -->
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-tag label="Label" size="small"></f-tag>
+            </f-div>
+            <!--End : slab-right -->
           </f-div>
-        <!--End : menu-list -->
-        </f-div>`
+        <!--End : single-line slab -->
+    `
 	});
-export const text_icon_button = () =>
+
+export const SingleLineSlab04 = () =>
 	defineComponent({
-		template: /*html*/ `   <f-div padding="large" align="middle-center">
-        <!--Start : menu-list -->
-          <f-div width="220px" state="secondary" padding="medium" gap="medium" border="small solid default bottom" clickable>
-            <!--Start : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-left" gap="medium" align="middle-left">
-              <f-text variant="para" size="medium" weight="regular">Text</f-text>
+		template: /*html*/ `
+            <!--Start : single-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium" align="middle-left">
+             <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon"></f-icon>
             </f-div>
-            <!--End : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <!--Start : menu-list-section-right : This data-f-id=\"menu-list-section-right\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-right" height="hug-content" width="hug-content">
-                <f-icon-button icon="i-icon" variant="block" type="packed" state="inherit" size="small"></f-icon-button>
+             <!--SEnd : slab-left -->
+             <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" width="fill-container" gap="medium" align="middle-left">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
             </f-div>
-            <!--End : menu-list-section-right : This data-f-id=\"menu-list-section-right\" is swappable, Find examples in \`Swappable items\`  -->
+            <!--End : slab-middle  -->
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-text variant="para" size="small" weight="regular">Text</f-text>
+            </f-div>
+            <!--End : slab-right -->
           </f-div>
-        <!--End : menu-list -->
-        </f-div>`
+        <!--End : single-line slab -->
+    `
 	});
-export const icon_text_icon_button = () =>
+
+export const MultiLineSlab01 = () =>
 	defineComponent({
-		template: /*html*/ `   <f-div padding="large" align="middle-center">
-        <!--Start : menu-list -->
-          <f-div width="220px" state="secondary" padding="medium" gap="medium" border="small solid default bottom" clickable>
-            <!--Start : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-left" gap="medium" align="middle-left">
-                <f-icon size="small" source="i-icon"></f-icon>
-              <f-text variant="para" size="medium" weight="regular">Text</f-text>
+		template: /*html*/ `
+            <!--Start : multi-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium">
+            <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon" size="x-large"></f-icon>
             </f-div>
-            <!--End : menu-list-section-left : This data-f-id=\"menu-list-section-left\" is swappable, Find examples in \`Swappable items\`  -->
-            <!--Start : menu-list-section-right : This data-f-id=\"menu-list-section-right\" is swappable, Find examples in \`Swappable items\`  -->
-            <f-div data-f-id="menu-list-section-right" height="hug-content" width="hug-content">
-                <f-icon-button icon="i-icon" variant="block" type="packed" state="inherit" size="small"></f-icon-button>
+            <!--End : slab-left -->
+            <f-div width="fill-container" gap="x-small" direction="column">
+            <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" direction="row" gap="medium" align="middle-left" width="fill-container">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
+              <f-div gap="medium" width="hug-content">
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+                <f-icon-button
+                  icon="i-icon"
+                  variant="block"
+                  type="packed"
+                  size="small"
+                  state="inherit"
+                ></f-icon-button>
+              </f-div>
+              </f-div>
+                <!--End : slab-middle  -->
+                <!--Start : section-subtext  -->
+                <f-text variant="para" size="medium" weight="regular" :inline="true">Description</f-text>
+                <!--End : section-subtext -->
             </f-div>
-            <!--End : menu-list-section-right : This data-f-id=\"menu-list-section-right\" is swappable, Find examples in \`Swappable items\`  -->
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+            </f-div>
+            <!--End : slab-right -->
           </f-div>
-        <!--End : menu-list -->
-        </f-div>`
+        <!--End : multi-line slab -->
+    `
+	});
+
+export const MultiLineSlab02 = () =>
+	defineComponent({
+		template: /*html*/ `
+             <!--Start : multi-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium">
+            <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon" size="x-large"></f-icon>
+            </f-div>
+            <!--End : slab-left -->
+            <f-div width="fill-container" gap="x-small" direction="column">
+            <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" direction="row" gap="medium" align="middle-left" width="fill-container">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
+              <f-div gap="medium" width="hug-content">
+               <f-counter label="88"></f-counter>
+              </f-div>
+              </f-div>
+                <!--End : slab-middle  -->
+                <!--Start : section-subtext  -->
+                <f-text variant="para" size="medium" weight="regular" :inline="true">Description</f-text>
+                <!--End : section-subtext -->
+            </f-div>
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+            </f-div>
+            <!--End : slab-right -->
+          </f-div>
+        <!--End : multi-line slab -->
+    `
+	});
+
+export const MultiLineSlab03 = () =>
+	defineComponent({
+		template: /*html*/ `
+       <!--Start : multi-line slab -->
+          <f-div width="382px" state="secondary" padding="medium" gap="medium">
+            <!--Start : slab-left -->
+            <f-div data-f-id="slab-left" height="hug-content" width="hug-content">
+              <f-icon source="i-icon" size="x-large"></f-icon>
+            </f-div>
+            <!--End : slab-left -->
+            <f-div width="fill-container" gap="x-small" direction="column">
+            <!--Start : slab-middle  -->
+            <f-div data-f-id="slab-middle" direction="row" gap="medium" align="middle-left" width="fill-container">
+              <f-text variant="heading" size="small" weight="bold" :inline="true">Heading</f-text>
+              <f-div gap="medium" width="hug-content">
+                <f-tag label="Label" size="small"></f-tag>
+              </f-div>
+              </f-div>
+                <!--End : slab-middle  -->
+                <!--Start : section-subtext  -->
+                <f-text variant="para" size="medium" weight="regular" :inline="true">Description</f-text>
+                <!--End : section-subtext -->
+            </f-div>
+            <!--START : slab-right -->
+            <f-div data-f-id="slab-right" width="hug-content" gap="medium">
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+              <f-icon-button
+                icon="i-icon"
+                variant="block"
+                type="packed"
+                size="medium"
+                state="inherit"
+              ></f-icon-button>
+            </f-div>
+            <!--End : slab-right -->
+          </f-div>
+        <!--End : multi-line slab -->
+           `
 	});
---
+++ stories/topnav.stories.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { defineComponent } from "vue";
+
+export default {
+	title: "Vue Top Nav",
+	parameters: {
+		docs: {
+			inlineStories: false,
+			iframeHeight: 300
+		}
+	}
+};
