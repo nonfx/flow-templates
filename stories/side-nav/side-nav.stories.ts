4cc4716b4574e56e7c3b612de065dd21e0b43d38
Mayank fulera <fuleramayank@gmail.com>
2023-04-05 16:16:10 +0530

    story ts

--- stories/side-nav/side-nav.stories.ts
+++ stories/side-nav/side-nav.stories.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import sideNavCode from "./side-nav-code";
 import SideNav011 from "./SideNav01.vue";
+import { Story } from "@storybook/web-components";
 
 export default {
 	title: "Side Nav",
@@ -12,7 +13,7 @@ export default {
 	}
 };
 
-const Template = () => {
+const Template: Story<any> = () => {
 	return SideNav011;
 };
 
