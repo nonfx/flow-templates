b35205469422388f1bee4e1b1b3a6c3809afcc49
Mayank fulera <fuleramayank@gmail.com>
2023-05-23 10:16:31 +0530

    banner and templates

---
+++ stories/banner/Banner04.vue
@@ -0,0 +1,50 @@
+<template>
+	<f-div direction="column" gap="large">
+		<!-- Please Copy/paste code from here -->
+		<!-- Iteration has been used here just for demo purpose -->
+		<f-div
+			v-for="item in states"
+			:key="item"
+			:id="item"
+			padding="medium"
+			gap="auto"
+			:state="item"
+			align="middle-left"
+		>
+			<f-div gap="medium" align="middle-left" state="inherit">
+				<f-icon source="i-icon" size="medium" state="inherit"></f-icon>
+				<f-text variant="heading" size="medium" weight="regular" state="inherit"
+					>Your notification text goes here.</f-text
+				></f-div
+			>
+			<f-div width="hug-content" align="middle-left">
+				<f-icon-button
+					variant="block"
+					category="packed"
+					icon="i-close"
+					size="small"
+					state="neutral"
+					@click="actionButton"
+				></f-icon-button>
+			</f-div>
+		</f-div>
+		<!-- End Snippet -->
+	</f-div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	data() {
+		return {
+			states: ["primary", "success", "danger", "warning"]
+		};
+	},
+	methods: {
+		actionButton() {
+			console.log("Action Button Clicked!!!");
+		}
+	}
+});
+</script>
---
+++ stories/search/Search01.vue
@@ -0,0 +1,80 @@
+<template>
+	<f-div align="middle-center">
+		<f-div width="hug-content" align="middle-left" height="hug-content">
+			<f-icon-button
+				icon="i-search"
+				variant="round"
+				category="fill"
+				state="neutral"
+				size="small"
+				class="search-icon-collapsed"
+				v-if="expandedSearch === false"
+				@click="searchExpand"
+			></f-icon-button>
+			<f-div
+				width="168px"
+				class="search-expanded"
+				:data-expanded-search="expandedSearch"
+				overflow="hidden"
+				align="middle-left"
+			>
+				<f-search
+					size="small"
+					variant="round"
+					placeholder="Enter search keyword"
+					@focusout="closeExpandedSearch"
+					:value="searchValue"
+				></f-search>
+			</f-div>
+		</f-div>
+	</f-div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	data() {
+		return {
+			expandedSearch: false,
+			searchValue: ""
+		};
+	},
+	methods: {
+		searchExpand() {
+			this.expandedSearch = true;
+		},
+		closeExpandedSearch() {
+			if (this.expandedSearch) {
+				this.expandedSearch = false;
+			}
+		}
+	}
+});
+</script>
+
+<!-- style custom css-->
+<style lang="scss">
+@keyframes fadeIn {
+	0% {
+		opacity: 0;
+	}
+	100% {
+		opacity: 1;
+	}
+}
+.search-expanded {
+	&[data-expanded-search="false"] {
+		width: 0px !important;
+		transition: width 0.15s ease-out !important;
+	}
+	&[data-expanded-search="true"] {
+		width: 168px !important;
+		transition: width 0.15s ease-out !important;
+	}
+}
+
+.search-icon-collapsed {
+	animation: fadeIn 1s;
+}
+</style>
